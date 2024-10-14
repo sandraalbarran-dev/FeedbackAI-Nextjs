68513fb0ad201fc9197c971aee2a4255aec36f06
CalmHelper <oliver210428@gmail.com>
2024-10-14 15:12:04 +0500

    models and schemas defined
    Models defined in mongoose and schemas defined for validation using zod

---
+++ src/model/User.ts
@@ -0,0 +1,58 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { Message, MessageSchema } from "./Message";
+
+export interface User extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[];
+}
+
+const UserSchema: Schema<User> = new Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"],
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [
+      /^[\w.-]+@[\w.-]+\.[a-zA-Z]{2,}$/,
+      "Please use a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  verifyCode: {
+    type: String,
+    required: [true, "VerifyCode is required"],
+  },
+  verifyCodeExpiry: {
+    type: Date,
+    required: [true, "Verify Code Expiry is required"],
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  isAcceptingMessage: {
+    type: Boolean,
+    default: true,
+  },
+  messages: [MessageSchema],
+});
+
+const UserModel =
+  (mongoose.models.Users as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
